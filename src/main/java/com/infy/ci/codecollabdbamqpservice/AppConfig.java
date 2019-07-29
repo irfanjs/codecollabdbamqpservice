db5406c6dd9c008d4144ff0ab307a5be99dc4222
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-29 16:31:02 +0530

    code

---
+++ src/main/java/com/infy/ci/codecollabdbamqpservice/AppConfig.java
@@ -0,0 +1,10 @@
+package com.infy.ci.codecollabdbamqpservice;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.infy.ci.codecollabdbamqpservice" })
+public class AppConfig {
+
+}
