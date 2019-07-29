69322fdbe0e79fefdd6ecd2cb0d82a23d7347899
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-29 16:15:32 +0530

    code

---
+++ src/main/java/com/infy/ci/codecollabdbamqpservice/CodecollabdbamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.codecollabdbamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecollabdbamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecollabdbamqpserviceApplication.class, args);
+	}
+
+}
